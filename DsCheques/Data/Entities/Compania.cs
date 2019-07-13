66938558f08877a99ab1636c31bbfb4bcc5104cd
Carlos DAgostino <dagsis@dagsis.com.ar>
2019-07-13 13:53:22 -0300

    Companias 1

---
+++ DsCheques/Data/Entities/Compania.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DsCheques.Data.Entities
+{
+    public class Compania
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Campo Requerido")]
+        [Display(Name="Razón Social")]
+        [Column(TypeName = "varchar(50)")]
+
+        public string Name { get; set; }
+    }
+}
---
+++ DsCheques/Migrations/20190713163618_baseinicial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DsCheques.Migrations
+{
+    public partial class baseinicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companias", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companias");
+        }
+    }
+}
