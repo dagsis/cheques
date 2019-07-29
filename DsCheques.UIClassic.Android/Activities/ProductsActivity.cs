1586ff0b5480d9792993c562c4b614a02e207a0a
Carlos DAgostino <dagsis@dagsis.com.ar>
2019-07-29 15:46:14 -0300

    Classic Android 1

---
+++ DsCheques.UIClassic.Android/Activities/ProductsActivity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using DsCheques.Common.Models;
+using DsCheques.Common.Services;
+using DsCheques.UIClassic.Android.Adapters;
+using DsCheques.UIClassic.Android.Helpers;
+using Newtonsoft.Json;
+
+namespace DsCheques.UIClassic.Android.Activities
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class ProductsActivity : AppCompatActivity
+    {
+        private TokenResponse token;
+        private string email;
+        private ApiService apiService;
+        private ListView productsListView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            this.SetContentView(Resource.Layout.ChequesPage);
+
+            this.productsListView = FindViewById<ListView>(Resource.Id.productsListView);
+
+            this.email = Intent.Extras.GetString("email");
+            var tokenString = Intent.Extras.GetString("token");
+            this.token = JsonConvert.DeserializeObject<TokenResponse>(tokenString);
+
+            this.apiService = new ApiService();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            var response = await this.apiService.GetListAsync<Cheque>(
+                "http://www.dscheques.ferozo.net",
+                "/api",
+                "/Cheques/" + this.email,
+                "bearer",
+                this.token.Token);
+
+            if (!response.IsSuccess)
+            {
+                DiaglogService.ShowMessage(this, "Error", response.Message, "Accept");
+                return;
+            }
+
+            var products = (List<Cheque>)response.Result;
+            this.productsListView.Adapter = new ChequesListAdapter(this, products);
+            this.productsListView.FastScrollEnabled = true;
+        }
+    }
+
+}
---
+++ DsCheques.UIClassic.Android/Adapters/ChequesListAdapter.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Views;
+using Android.Widget;
+using DsCheques.UIClassic.Android.Helpers;
+using System.Collections.Generic;
+using DsCheques.Common.Models;
+
+
+namespace DsCheques.UIClassic.Android.Adapters
+{
+    public class ChequesListAdapter : BaseAdapter<Cheque>
+    {
+        private readonly List<Cheque> items;
+        private readonly Activity context;
+
+        public ChequesListAdapter(Activity context, List<Cheque> items) : base()
+        {
+            this.context = context;
+            this.items = items;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override Cheque this[int position] => items[position];
+
+        public override int Count => items.Count;
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var item = items[position];
+
+            var imageBitmap = ImageHelper.GetImageBitmapFromUrl(item.ImageFullPath);
+
+            if (convertView == null)
+            {
+                convertView = context.LayoutInflater.Inflate(Resource.Layout.ChequeRow, null);
+            }
+
+            convertView.FindViewById<TextView>(Resource.Id.nameTextView).Text = item.Cliente.Name ;
+            convertView.FindViewById<TextView>(Resource.Id.priceTextView).Text = $"{item.Importe:C2}";
+            convertView.FindViewById<ImageView>(Resource.Id.chequeImageView).SetImageBitmap(imageBitmap);
+
+            return convertView;
+        }
+    }
+
+}
